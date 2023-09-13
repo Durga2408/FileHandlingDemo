b192a42458544a511bf7c8a7615874230ed77a33
Durga2408 <144308242+Durga2408@users.noreply.github.com>
2023-09-13 11:20:57 +0530

    Add files via upload

---
+++ fileHandlingDemo.py
@@ -0,0 +1,6 @@
+import os
+
+# os.mkdir("d:\\FileHandling")
+print("all files in filehandiling are :",os.listdir("d:\\FileHandling"))
+os.removedirs("d:\\FileHandling")
+print("all files in filehandiling are :",os.listdir("d:\\FileHandling"))
